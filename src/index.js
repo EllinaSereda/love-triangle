e8c12a17138c9f7ac8a5161430368d4771a3e18c
Ellina <ellinasereda18@gmail.com>
2019-03-22 10:01:02 +0300

    feat: implement love-triangle function

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let intriangles=[];
+  let triangles=0;
+  preferences.forEach((v,i,a)=>{
+    if (intriangles.every(z=>z!=v)){
+      let second=a[v-1];
+      let third=a[second-1];
+      if (third==(i+1)&&second!=third&&v!=second&&v!=third){
+        intriangles.push(v,second,third);
+        triangles++;
+      }
+      
+    }
+  })
+  return triangles;
 };
